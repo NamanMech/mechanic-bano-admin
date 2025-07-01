b22977650edb267a21de076e21b075794b723198
NamanMech <mechanicseekho@gmail.com>
2025-07-02 04:49:57 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Home from './pages/Home';
 import YouTubeVideoManagement from './pages/YouTubeVideoManagement';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 
 export default function App() {
   return (
-    <Router>
-      <div className="p-4">
-        <nav className="mb-4 space-x-4">
-          <Link to="/" className="text-blue-500">Home</Link>
-          <Link to="/youtube" className="text-blue-500">YouTube Video Management</Link>
-        </nav>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/youtube" element={<YouTubeVideoManagement />} />
-        </Routes>
-        <ToastContainer />
-      </div>
-    </Router>
-  )
+    <div>
+      <h1 className="text-3xl font-bold text-center mt-4">Mechanic Bano Admin Panel</h1>
+      <YouTubeVideoManagement />
+    </div>
+  );
 }
