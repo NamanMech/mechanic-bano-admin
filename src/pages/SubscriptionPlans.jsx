69dfbb69b774e5a97a38e87694ea2dff21e66a17
NamanMech <mechanicseekho@gmail.com>
2025-07-06 16:03:34 +0530

    Update SubscriptionPlans.jsx

--- src/pages/SubscriptionPlans.jsx
+++ src/pages/SubscriptionPlans.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import Spinner from '../components/Spinner';
+import Spinner from '../components/Spinner.jsx';
 
 export default function SubscriptionPlans() {
   const [plans, setPlans] = useState([]);
