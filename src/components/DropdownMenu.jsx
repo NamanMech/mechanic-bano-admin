1135c5acd8713f0b2e57c7bfc97eba809d6d41bf
NamanMech <mechanicseekho@gmail.com>
2025-07-08 12:36:07 +0530

    Update DropdownMenu.jsx

--- src/components/DropdownMenu.jsx
+++ src/components/DropdownMenu.jsx
@@ -52,10 +52,10 @@ export default function DropdownMenu({ user, onEdit, onDelete, onExpire, process
   return (
     <>
       <button ref={buttonRef} onClick={handleToggleMenu} className="dropdown-trigger">
-        <svg width="24" height="24" viewBox="0 0 24 24" fill="white" xmlns="http://www.w3.org/2000/svg">
-          <path d="M4 6h16M4 12h16M4 18h16" stroke="currentColor" strokeWidth="2" strokeLinecap="round" />
-        </svg>
-      </button>
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M4 6h16M4 12h16M4 18h16" stroke="currentColor" strokeWidth="2" strokeLinecap="round" />
+  </svg>
+</button>
       {isOpen && createPortal(menu, document.body)}
     </>
   );
