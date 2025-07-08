6b81e152cbf35a8797c97ed4c9148a2b3eb9660f
NamanMech <mechanicseekho@gmail.com>
2025-07-09 00:42:54 +0530

    Create cloudinaryConfig.js

---
+++ src/utils/cloudinaryConfig.js
@@ -0,0 +1,3 @@
+// src/utils/cloudinaryConfig.js
+export const CLOUDINARY_UPLOAD_URL = `https://api.cloudinary.com/v1_1/mechanicbanocloud/raw/upload`;
+export const CLOUDINARY_UPLOAD_PRESET = 'unsigned_preset'; // We'll configure this next in Cloudinary dashboard
