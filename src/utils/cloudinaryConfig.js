dec9d91ba690d69bb0fb51d1b9f34e98fcf1a6d0
NamanMech <mechanicseekho@gmail.com>
2025-07-09 16:51:06 +0530

    Delete src/utils/cloudinaryConfig.js

--- src/utils/cloudinaryConfig.js
+++
@@ -1,3 +0,0 @@
-// src/utils/cloudinaryConfig.js
-export const CLOUDINARY_UPLOAD_URL = `https://api.cloudinary.com/v1_1/mechanicbanocloud/raw/upload`;
-export const CLOUDINARY_UPLOAD_PRESET = 'unsigned_preset'; // We'll configure this next in Cloudinary dashboard
