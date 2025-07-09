033a283f9c72cd366b1861d5a704e481a4cc0fbc
NamanMech <mechanicseekho@gmail.com>
2025-07-09 23:53:58 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['pdfjs-dist'], // Add this line
-  },
-  build: {
-    commonjsOptions: {
-      transformMixedEsModules: true, // Add this line
-    },
-    rollupOptions: {
-      external: [
-        /pdfjs-dist/,
-      ]
-    }
-  }
+  // Remove all PDF.js specific config
 })
