19548e4d748e7b8007982ec9721c586aa95973ad
NamanMech <mechanicseekho@gmail.com>
2025-09-02 23:29:54 +0530

    Update supabaseClient.js

--- src/utils/supabaseClient.js
+++ src/utils/supabaseClient.js
@@ -1,7 +1,7 @@
-// src/utils/supabaseClient.js
+// supabaseClient.js - Updated
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://owmdhrvscnbiuvoihozb.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im93bWRocnZzY25iaXV2b2lob3piIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIwNTc3NzAsImV4cCI6MjA2NzYzMzc3MH0.uff-HrH8DnOC8VGffBSYXzmV_Ur2JQWnHDM0woudSG4';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
+);
