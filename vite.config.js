d99a1a5f83d558da9d1c78bb35e3181653b78b0a
NamanMech <mechanicseekho@gmail.com>
2025-09-02 23:34:48 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,20 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [],
+  plugins: [react()], // React plugin add karein
   build: {
     rollupOptions: {
       output: {
         manualChunks(id) {
           if (id.includes('pdf.worker.min.js')) {
-            return 'pdfjs'; // Custom chunk for the worker file
+            return 'pdfjs';
           }
         }
       }
     }
+  },
+  server: {
+    port: 3000,
+    open: true
   }
-});
+})
