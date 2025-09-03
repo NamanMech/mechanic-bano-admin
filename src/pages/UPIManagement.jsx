85d8e5fb5e8febe8a05f2e2ca6be768e52ebaebc
NamanMech <mechanicseekho@gmail.com>
2025-09-03 20:15:53 +0530

    Update UPIManagement.jsx

--- src/pages/UPIManagement.jsx
+++ src/pages/UPIManagement.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Spinner from '../components/Spinner';
 import { toast } from 'react-toastify';
+import '../styles/pages/upi-management.css';
 
 export default function UPIManagement() {
   const [upiId, setUpiId] = useState('');
